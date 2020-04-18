c2784f255873a4455ac70a4e516e5bb5e3b95ec9
Alvin Coulson <aaacoulson@gmail.com>
2020-04-18 02:02:29 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -4,9 +4,9 @@ class Application
     req = Rack::Request.new(env)
     
     if req.path.match(/items/)
-      item_name = req.path.match(/items/)[-1]
+      item_name = req.path.match(/items/).last
       if item = @@items.find {|item| item.name == item_name}
-        resp.write item.price
+        resp.write "#{item.price}"
       else
         resp.status = 400
         resp.write "Item not found"
