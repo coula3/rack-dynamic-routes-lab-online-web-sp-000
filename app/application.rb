040db218ead0908a818ff44cadbc1858551d876f
Alvin Coulson <aaacoulson@gmail.com>
2020-04-18 02:14:27 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,4 +1,4 @@
-require 'pry'
+
 class Application
   def call(env)
     resp = Rack::Response.new
@@ -8,7 +8,6 @@ class Application
       item_name = req.path.split("/items/")[-1]
       if item = @@items.detect {|item| item.name == item_name}
         resp.write "#{item.price}"
-        binding.pry
       else
         # resp.status = 400
         resp.write "Item not found"
